77c81402043b92bacc2b7166ca04de6161d89ed1
Andriy Makarov <angeltek69@gmail.com>
2025-03-07 09:53:45 +0100

    Initial commit

---
+++ src/main/java/com/academy/learning_journal_team3/LearningJournalTeam3Application.java
@@ -0,0 +1,13 @@
+package com.academy.learning_journal_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJournalTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJournalTeam3Application.class, args);
+	}
+
+}
