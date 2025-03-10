6c153f5b5b2b6b0c71179db1cd721b922a14d7c6
Patrick Bruckner <Patrick.Bruckner@tn.mischok.academy>
2025-03-10 13:37:49 +0100

    Klassen erstellt Repositories erstellt

---
+++ src/main/java/com/academy/learning_journal_team3/entity/Topic.java
@@ -0,0 +1,28 @@
+package com.academy.learning_journal_team3.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "topic")
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "topicList")
+    private List<TeachingClass> teachingClasses;
+
+}
