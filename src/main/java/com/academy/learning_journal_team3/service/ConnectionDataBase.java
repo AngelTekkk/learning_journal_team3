bbd0a58db7e06581c7b9af1ba3c1bb93b639dbb4
Verena Nowak <verena.nowak@tn.mischok.academy>
2025-03-17 08:28:34 +0100

    h2 Server + ConnectionDataBase + adding jdbc

--- src/main/java/com/academy/learning_journal_team3/service/ConnectionDataBase.java
+++ src/main/java/com/academy/learning_journal_team3/service/ConnectionDataBase.java
@@ -1,21 +1,21 @@
-//package com.academy.learning_journal_team3.service;
-//
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.stereotype.Service;
-//import java.util.List;
-//import java.util.Map;
-//
-//@Service
-//public class ConnectionDataBase {
-//    private final JdbcTemplate jdbcTemplate;
-//        public ConnectionDataBase (JdbcTemplate jdbcTemplate){
-//            this.jdbcTemplate = jdbcTemplate;
-//        }
-//        public List<Map<String, Object>> getAllEntries() {
-//            return jdbcTemplate.queryForList("SELECT * FROM TEST_TABLE");
-//
-//        }public void insertEntry(int id, String name){
-//                jdbcTemplate.update("INSERT INTO TEST_TABLE (ID, NAME) VALUES (?,?)", id, name);
-//            }
-//        }
+package com.academy.learning_journal_team3.service;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ConnectionDataBase {
+    private final JdbcTemplate jdbcTemplate;
+        public ConnectionDataBase (JdbcTemplate jdbcTemplate){
+            this.jdbcTemplate = jdbcTemplate;
+        }
+        public List<Map<String, Object>> getAllEntries() {
+            return jdbcTemplate.queryForList("SELECT * FROM TEST_TABLE");
+
+        }public void insertEntry(int id, String name){
+                jdbcTemplate.update("INSERT INTO TEST_TABLE (ID, NAME) VALUES (?,?)", id, name);
+            }
+        }
 
