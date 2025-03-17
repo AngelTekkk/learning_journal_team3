3dee631ee6f2f4184bea280c7ddcacd984822346
Patrick Bruckner <Patrick.Bruckner@tn.mischok.academy>
2025-03-17 15:20:20 +0100

    teachingclass erstellen, zur datenbank hinzufügen + liste der teachingclasses wird angezeigt
    unter ...8080/teachingClass

--- src/main/java/com/academy/learning_journal_team3/service/TeachingClassService.java
+++ src/main/java/com/academy/learning_journal_team3/service/TeachingClassService.java
@@ -1,12 +1,13 @@
 package com.academy.learning_journal_team3.service;
 import com.academy.learning_journal_team3.entity.TeachingClass;
-import com.academy.learning_journal_team3.entity.Topic;
+import com.academy.learning_journal_team3.model.TeachingclassModel;
 import com.academy.learning_journal_team3.repository.TeachingClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TeachingClassService {
@@ -22,4 +23,21 @@ public class TeachingClassService {
     public TeachingClass getTeachingClass(Long teachingClassID){
         return teachingClassRepository.findById(teachingClassID).get();
     }
+
+    public List<TeachingClass> getAllClasses() {
+        return teachingClassRepository.findAll();
+    }
+
+    public Long createTeachingclass(TeachingclassModel teachingclassModel){
+        for(TeachingClass currentTeachingClass : teachingClassRepository.findAll()){
+            if(currentTeachingClass.getName().equals(teachingclassModel.name())){
+                throw new RuntimeException("Klassenzimmer existiert bereits");
+            }
+        }
+        TeachingClass teachingclass = teachingClassRepository.save(TeachingClass.builder()
+                .name(teachingclassModel.name())
+                .build());
+        return teachingclass.getId();
+    }
+
 }
