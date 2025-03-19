a379f459c6955282ed98636ddf393e7bfdd7e297
Andriy Makarov <angeltek69@gmail.com>
2025-03-19 14:52:14 +0100

    Login and registration update

--- src/main/java/com/academy/learning_journal_team3/controller/UserController.java
+++ src/main/java/com/academy/learning_journal_team3/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.academy.learning_journal_team3.controller;
 
 import com.academy.learning_journal_team3.entity.User;
+import com.academy.learning_journal_team3.model.CustomUserDetails;
 import com.academy.learning_journal_team3.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -17,11 +21,24 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/registration")
-    public String registrationPage(Authentication authentication) {
-        System.out.println(authentication.getDetails());
-        System.out.println(authentication.getAuthorities());
-        System.out.println(authentication.getCredentials());
-        System.out.println(authentication.getPrincipal());
+    public String registrationPage(Authentication authentication, Model model) {
+        boolean isUsers = userService.getAllUsers().isEmpty();
+        model.addAttribute("isUsers", isUsers);
+        if (authentication != null) {
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            User currentUser = userService.findByEmail(userDetails.getUsername());
+            model.addAttribute("user", currentUser);
+            model.addAttribute("editMode", true);
+        } else {
+            model.addAttribute("user", new User());
+            model.addAttribute("editMode", false);
+        }
+//        boolean isAdmin = authentication != null &&
+//                authentication.getAuthorities().stream()
+//                        .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+//
+//        model.addAttribute("isAdmin", isAdmin);
+
         return "registration";
     }
 
