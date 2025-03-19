c574d931498762d6dae70707e360c9e3b84ad5c7
Verena Nowak <verena.nowak@tn.mischok.academy>
2025-03-19 10:25:47 +0100

    Funktionierende Datenbank

---
+++ src/main/java/com/academy/learning_journal_team3/service/Person.java
@@ -0,0 +1,28 @@
+//package com.academy.learning_journal_team3.service;
+//
+//public class Person {
+//    private int id;
+//    private String first_name;
+//    private String last_name;
+//
+//    public int getId() {
+//        return this.id;
+//    }
+//    public String getFirst_name() {
+//        return this.first_name;
+//    }
+//    public String getFirst_name() {
+//        return this.last_name;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setFirst_name(String first_name) {
+//        this.first_name = first_name;
+//    }
+//
+//    public void setLast_name(String last_name) {
+//        this.last_name = last_name;
+//    }
+//}
