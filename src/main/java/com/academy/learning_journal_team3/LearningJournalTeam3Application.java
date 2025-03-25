ac534e402e9ab159764660d099d4c4503ff9374a
Andriy Makarov <angeltek69@gmail.com>
2025-03-25 09:49:24 +0100

    First step for adding Datestamp in teachingclass_topic

--- src/main/java/com/academy/learning_journal_team3/LearningJournalTeam3Application.java
+++ src/main/java/com/academy/learning_journal_team3/LearningJournalTeam3Application.java
@@ -1,6 +1,5 @@
 package com.academy.learning_journal_team3;
 
-import com.academy.learning_journal_team3.entity.Entry;
 import com.academy.learning_journal_team3.entity.TeachingClass;
 import com.academy.learning_journal_team3.entity.Topic;
 import com.academy.learning_journal_team3.entity.User;
@@ -21,41 +20,39 @@ public class LearningJournalTeam3Application {
 				run(LearningJournalTeam3Application.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner demo(TeachingClassRepository teachingClassRepository, UserRepository userRepository, TopicsRepository topicsRepository, EntryRepository entryRepository){
-//		return (args) -> {
-//			System.out.println("Hallöchen");
-//
-//
-//			TeachingClass teachingClass1 = TeachingClass.builder().name("Schulklasse 1").build();
-//			TeachingClass teachingClass2 = TeachingClass.builder().name("Schulklasse 2").build();
-//			TeachingClass teachingClass3 = TeachingClass.builder().name("Schulklasse 3").build();
-//			teachingClassRepository.save(teachingClass1);
-//			teachingClassRepository.save(teachingClass2);
-//			teachingClassRepository.save(teachingClass3);
-//
-//
-//			User user1 = User.builder().firstName("Markus").teachingClass(teachingClass1).build();  // builder erstetzt die hartgecodete Variante mit der erstellung der Liste
-//			userRepository.save(user1);
-//			User user2 = User.builder().firstName("Lisa").teachingClass(teachingClass1).build();
-//			userRepository.save(user2);
-//			User user3 = User.builder().firstName("Verona").teachingClass(teachingClass2).build();
-//			userRepository.save(user3);
-//			User user4 = User.builder().firstName("Paul").teachingClass(teachingClass3).build();
-//			userRepository.save(user4);
-//
-//
-//			Topic topic1 = Topic.builder().name("SQL").build();
-//			Topic topic2 = Topic.builder().name("Java").build();
-//			Topic topic3 = Topic.builder().name("BWL").build();
-//			Topic topic4 = Topic.builder().name("Netzwerktechnik").build();
-//			topicsRepository.save(topic1);
-//			topicsRepository.save(topic2);
-//			topicsRepository.save(topic3);
-//			topicsRepository.save(topic4);
-//
-//		};
-//	}
+	@Bean
+	public CommandLineRunner demo(TeachingClassRepository teachingClassRepository, UserRepository userRepository, TopicsRepository topicsRepository, EntryRepository entryRepository){
+		return (args) -> {
+			System.out.println("Hallöchen");
+
+			TeachingClass teachingClass1 = TeachingClass.builder().name("Schulklasse 1").build();
+			TeachingClass teachingClass2 = TeachingClass.builder().name("Schulklasse 2").build();
+			TeachingClass teachingClass3 = TeachingClass.builder().name("Schulklasse 3").build();
+			teachingClassRepository.save(teachingClass1);
+			teachingClassRepository.save(teachingClass2);
+			teachingClassRepository.save(teachingClass3);
+
+			User user1 = User.builder().firstName("Andrey").lastName("Makarov").email("am@am.com")
+					.password("123456Am").role("ADMIN").build();
+			userRepository.save(user1);
+			User user2 = User.builder().firstName("Franziska").lastName("Seidl").email("fs@fs.com")
+					.password("123456Fs").role("USER").build();
+			userRepository.save(user2);
+			User user3 = User.builder().firstName("Verena").lastName("Nowak").email("vn@vn.com")
+					.password("123456Vn").role("USER").build();
+			userRepository.save(user3);
+
+			Topic topic1 = Topic.builder().name("SQL").build();
+			Topic topic2 = Topic.builder().name("Java").build();
+			Topic topic3 = Topic.builder().name("BWL").build();
+			Topic topic4 = Topic.builder().name("Netzwerktechnik").build();
+			topicsRepository.save(topic1);
+			topicsRepository.save(topic2);
+			topicsRepository.save(topic3);
+			topicsRepository.save(topic4);
+
+		};
+	}
 }
 
 
